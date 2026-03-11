3299e1d2cccd4732805fe0a57688feb1a31c55ec
Meli <ms.melcastro@gmail.com>
2026-03-11 09:18:47 -0700

    more js

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,4 +1,5 @@
 // ADD A FUNCTION CALLED CALCULATE
+
 function calculate(x, y, operation) {
 
     switch (operation) {
@@ -20,23 +21,21 @@ function calculate(x, y, operation) {
 }
 // COLLECT FIRST NUMBER FROM USER
 
-// COLLECT SECOND NUMBER FROM USER
-
-// COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
-
-// CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
-
-
-
 let result;
 let operation;
 
 do {
-
     let num1 = Number(prompt("Enter the first number:"));
+
+// COLLECT SECOND NUMBER FROM USER
+
     let num2 = Number(prompt("Enter the second number:"));
 
+// COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+
     operation = prompt("Enter an operation: add, subtract, multiply, or divide").toLowerCase();
+
+// CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
 
     result = calculate(num1, num2, operation);
 
