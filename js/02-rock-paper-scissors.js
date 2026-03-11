0725f96a5c758a113ed6eabdbecd64ee97b51ae7
Meli <ms.melcastro@gmail.com>
2026-03-11 09:28:54 -0700

    JS

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -1,30 +1,36 @@
-let userChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
-let computerChoice = Math.floor(Math.random() * 3);
+let userChoice;
+let computerChoice;
 
-if (computerChoice === 0) {
+do {
+
+    userChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
+
+    if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
+        alert("Invalid choice. Please enter rock, paper, or scissors.");
+    }
+
+} while (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors");
+
+let randomNum = Math.floor(Math.random() * 3);
+
+if (randomNum === 0) {
     computerChoice = "rock";
-} else if (computerChoice === 1) {
+} else if (randomNum === 1) {
     computerChoice = "paper";
 } else {
     computerChoice = "scissors";
 }
 
-if (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors") {
-    alert("Invalid choice. Please refresh and enter rock, paper, or scissors.");
-}
-
-else if (userChoice === computerChoice) {
+if (userChoice === computerChoice) {
     alert("It's a tie! You both chose " + userChoice);
-}
-
+} 
 else if (
     (userChoice === "rock" && computerChoice === "scissors") ||
     (userChoice === "paper" && computerChoice === "rock") ||
     (userChoice === "scissors" && computerChoice === "paper")
 ) {
     alert("You win! " + userChoice + " beats " + computerChoice);
-}
-
+} 
 else {
     alert("Computer wins! " + computerChoice + " beats " + userChoice);
 }
